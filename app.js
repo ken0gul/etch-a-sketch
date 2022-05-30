2c747374a4c0e9ab9a06305efc250668ff955f8e
ken0gul <ken0gul@gmail.com>
2022-05-30 18:15:07 -0400

    Add the feature to add 10% black in each mouseover

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 // Selecting the main container
 const container = document.querySelector(".container");
 
+// Global Variables
+let color = 0.10;
 
+
+// Global Function
+// Darken Function
+function darkenCells(e) {
+    
+    e.target.style.backgroundColor =`rgba(0,0,0,${color+= 0.03})`
+    if (e.target.style.backgroundColor ==`rgb(0, 0, 0)`) color = 0.10;
+}
 
 // Creating grid cells
 for (let i = 1; i <= 16; i++) {
      let cells = document.createElement('div');
+     cells.classList.add('cell');
      
-    container.appendChild(cells)
+    container.appendChild(cells);
 }
 
 
@@ -18,6 +29,8 @@ const button = `<button>Restart</button>`;
 container.insertAdjacentHTML("beforebegin", button);
 const erase = `<button class="erase">Erase</button>`;
 container.insertAdjacentHTML("beforebegin", erase);
+const darken = `<button class="darken">Darken</button>`;
+container.insertAdjacentHTML('beforebegin', darken)
 
 // Add an event listener
 
@@ -42,12 +55,39 @@ document.querySelector('button').addEventListener('click', ()=> {
         })
         document.querySelectorAll('div').forEach(div => {
     
-            div.addEventListener('mouseover', e => {
-                e.target.style.backgroundColor = `rgb(${Math.random()*255+1},${Math.random()*255 + 1},${Math.random()*255 +1})`
-            })
+            div.addEventListener('mouseover', randomHoverEffect);
             
-        
+            // // Hover effect loop
+            // div.addEventListener('transitionend', e=> {
+            //     if(e.propertyName === 'background-color') {
+            //         e.target.style.backgroundColor = `rgb(${Math.random()*255+1},${Math.random()*255 + 1},${Math.random()*255 +1})`;
+            //     }
+            // })
             // div.addEventListener('mouseout', e=> e.target.style.backgroundColor = 'black');
+
+            // Darken Effect
+
+
+          
+            const darkenEffect = document.querySelector('.darken');
+            if (color < 1) {
+
+                darkenEffect.addEventListener('click', (e) => {
+                    div.removeEventListener('mouseover', randomHoverEffect);
+                    div.style.backgroundColor = 'white';
+                    div.style.border = '1px solid black';
+                    
+                    div.addEventListener('mouseenter', darkenCells); 
+                    
+                    
+                    
+                           
+                    
+                })
+            } 
+
+           
+        
            
         })
     }
@@ -55,20 +95,51 @@ document.querySelector('button').addEventListener('click', ()=> {
     
 })
 
+
+// Creating Hover Effect Functionality
+function randomHoverEffect(e) {
+    e.target.style.backgroundColor = `rgba(${Math.random()*255+1},${Math.random()*255 + 1},${Math.random()*255 +1})`
+    e.target.style.transition = 'all 0.2s';
+}
+
 // Hover effect
 document.querySelectorAll('div').forEach(div => {
     
-    div.addEventListener('mouseover', e => {
-         e.target.style.backgroundColor = `rgb(${Math.random()*255+1},${Math.random()*255 + 1},${Math.random()*255 +1})`
-    })
+    
+    div.addEventListener('mouseover', randomHoverEffect);
+
+    // div.addEventListener('transitionend', randomHoverEffect)
     
 
     // div.addEventListener('mouseout', e=> e.target.style.backgroundColor = 'black');
-   
+   // Darken effect
+const darkenEffect = document.querySelector('.darken');
+darkenEffect.addEventListener('click', (e) => {
+    div.removeEventListener('mouseover', randomHoverEffect);
+    div.style.backgroundColor = 'white';
+    div.style.border = '1px solid black'
+
+    div.addEventListener('mouseenter', darkenCells); 
+
 })
+})
+
 
 
 // Erase button
 document.querySelector('.erase').addEventListener('click', ()=> {
-    document.querySelectorAll('div').forEach(div => div.style.backgroundColor = 'black');
+    document.querySelectorAll('div').forEach(div => {
+        div.style.backgroundColor = 'white';
+        div.style.border = '1px solid black'
+        // Stop transition when clicked
+        // Or it goes forever
+        div.addEventListener('transitionend', e=> {
+            e.target.style.transition = 'none';
+          
+        })
+
+        
+    }
+        );
 })
+
